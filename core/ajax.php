594f016aa493a4124e20bfe6fe428ec5d69b0027
Myrmal <34450441+Myrmal@users.noreply.github.com>
2018-06-15 19:55:00 +0300

    Update ajax.php

--- core/ajax.php
+++ core/ajax.php
@@ -7,9 +7,9 @@ $error = false;
 if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"])) {
     $error = true;
 } else {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 }
 
 if ($error)
